a60bba65ec226fc2c315404e51ddcdd6092f240c
Samuel Pereira <gmr.quest.10@gmail.com>
2023-04-15 21:09:04 -0300

    feat: create api request function

---
+++ src/enums/index.ts
@@ -0,0 +1,17 @@
+export enum Categories {
+  'CELLPHONE' = 'Celular',
+  'REFRIGERATOR' = 'Geladeira',
+  'TV' = 'TV',
+}
+
+export enum MercadoLivreCategoryLinks {
+  'Celular' = 'https://lista.mercadolivre.com.br/celulares-smartphones',
+  'Geladeira' = 'https://lista.mercadolivre.com.br/geladeiras-freezers',
+  'TV' = 'https://lista.mercadolivre.com.br/televisores',
+}
+
+export enum BuscapeCategoryLinks {
+  'Celular' = 'https://www.buscape.com.br/celular',
+  'Geladeira' = 'https://www.buscape.com.br/geladeira',
+  'TV' = 'https://www.buscape.com.br/tv',
+}
